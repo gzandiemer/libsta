7da71528aa0f089028d230c383581745cdcb9bbb
gzandiemer <g.z.diemer@gmail.com>
2019-11-17 16:45:51 +0100

    fix issues

--- models/book.js
+++ models/book.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
 const BookSchema = new mongoose.Schema({
-    // _id: {
-    //     type: Number,
-    //     required: true,
-    // },
+  
     title: {
         type: String,
         required: true,
@@ -17,7 +14,7 @@ const BookSchema = new mongoose.Schema({
     },
     lang: {
         type: String,
-        required: false,
+        required: true,
         minlength: 5
     },
     pubData: {
@@ -29,25 +26,25 @@ const BookSchema = new mongoose.Schema({
         type: mongoose.SchemaTypes.ObjectId,
         ref: 'Member',
         autopopulate: {
-            maxDepth:1
+            maxDepth: 1
         }
-    }], 
+    }],
     likers: [{
         type: mongoose.SchemaTypes.ObjectId,
         ref: 'Member',
         autopopulate: {
-            maxDepth:1
+            maxDepth: 1
         }
-    }], 
+    }],
     commentsReceived: [{
         //work on this
         type: mongoose.SchemaTypes.ObjectId,
         ref: 'Member',
         text: '',
         autopopulate: {
-            maxDepth:1
+            maxDepth: 1
         }
-    }], 
+    }],
     booked: Boolean
 })
 
@@ -56,4 +53,4 @@ BookSchema.plugin(require('mongoose-autopopulate'))
 const BookModel = mongoose.model('Book', BookSchema)
 
 module.exports = BookModel
-    
+
